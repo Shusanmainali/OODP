b4458713eb4ee26159368646b77d6055f2204f7d
Shusanmainali <167161455+Shusanmainali@users.noreply.github.com>
2026-01-06 20:03:58 +0545

    a

--- src/Workshop7/Student.java
+++ src/Workshop7/Student.java
@@ -1,5 +1,11 @@
 package Workshop7;
 
 public class Student {
+    private String name;
+    private int age;
 
+    public Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
 }
